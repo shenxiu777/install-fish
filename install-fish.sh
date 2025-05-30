47f55a56db7abaf4f6908a2ef2938d1256fa2acd
shenxiu777 <131572166+shenxiu777@users.noreply.github.com>
2025-05-30 18:09:36 +0800

    feat: also download config for root

--- install-fish.sh
+++ install-fish.sh
@@ -14,11 +14,14 @@ chsh -s /usr/bin/fish "$SUDO_USER"
 
 # Create fish config directory for the user if it doesn't exist
 mkdir -p "/home/$SUDO_USER/.config/fish"
-
 # Download fish configuration file
 curl -o "/home/$SUDO_USER/.config/fish/config.fish" https://raw.githubusercontent.com/shenxiu777/install-fish/refs/heads/main/config.fish
-
 # Ensure correct ownership of the config directory and file
 chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/fish"
+
+# Create fish config directory for the root user if it doesn't exist
+mkdir -p "/root/.config/fish"
+# Download fish configuration file for the root user
+curl -o "/root/.config/fish/config.fish" https://raw.githubusercontent.com/shenxiu777/install-fish/refs/heads/main/config.fish
 
 echo "Fish installed and configured successfully for $SUDO_USER!"
